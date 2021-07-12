52afe8dfb5b67489a2e7897af5164ed32b50989b
DevMedDim <mrakdimdev@gmail.com>
2021-07-12 13:46:59 +0100

    finished coding the Large widget

--- resources/js/components/Admin/MainContent/WidgetLarge.js
+++ resources/js/components/Admin/MainContent/WidgetLarge.js
@@ -2,6 +2,9 @@ import React from 'react'
 import "/resources/css/WidgetLarge.css";
 
 export default function WidgetLarge() {
+    const Button = ({type}) =>{
+        return <button className={"WidgetLargeButton " + type }>{type}</button>
+    }
     return (
         
         <div className="WidgetLarge">
@@ -17,12 +20,43 @@ export default function WidgetLarge() {
                 </tr>
                 <tr className="WidgetLargeTr">
                     <td className="WidgetLargeUser">
-                        <img src="https://images.pexels.com/photos/5474040/pexels-photo-5474040.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="WidgetLargeImg" />
+                        <img className="WidgetLargeImg" src="https://images.pexels.com/photos/5474040/pexels-photo-5474040.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""  />
                    <span className="WidgetLargeName">John Doe</span>
                     </td>
                     <td className="WidgetLargeDate">25 August 2021</td>
                     <td className="WidgetLargeAmount">$122.23</td>
-                    <td className="WidgetLargeStatus"></td>
+                    <td className="WidgetLargeStatus"><Button type="Approved" /></td>
+                    
+                </tr>
+                <tr className="WidgetLargeTr">
+                    <td className="WidgetLargeUser">
+                        <img className="WidgetLargeImg" src="https://images.pexels.com/photos/5474040/pexels-photo-5474040.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""  />
+                   <span className="WidgetLargeName">John Doe</span>
+                    </td>
+                    <td className="WidgetLargeDate">25 August 2021</td>
+                    <td className="WidgetLargeAmount">$122.23</td>
+                    <td className="WidgetLargeStatus"><Button type="Declined" /></td>
+                    
+                </tr>
+                <tr className="WidgetLargeTr">
+                    <td className="WidgetLargeUser">
+                        <img className="WidgetLargeImg" src="https://images.pexels.com/photos/5474040/pexels-photo-5474040.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""  />
+                   <span className="WidgetLargeName">John Doe</span>
+                    </td>
+                    <td className="WidgetLargeDate">25 August 2021</td>
+                    <td className="WidgetLargeAmount">$122.23</td>
+                    <td className="WidgetLargeStatus"><Button type="Pending" />
+</td>
+                                    </tr>
+                <tr className="WidgetLargeTr">
+                    <td className="WidgetLargeUser">
+                        <img className="WidgetLargeImg" src="https://images.pexels.com/photos/5474040/pexels-photo-5474040.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""  />
+                   <span className="WidgetLargeName">John Doe</span>
+                    </td>
+                    <td className="WidgetLargeDate">25 August 2021</td>
+                    <td className="WidgetLargeAmount">$122.23</td>
+                    <td className="WidgetLargeStatus"><Button type="Approved" /></td>
+                    
                 </tr>
             </table>
         </div>
